d4ebb3fc3fae49456882231c61cd9621bee0ec4f
Percossi283 <eros.percossi@gmail.com>
2024-09-18 19:20:11 +0200

    Add files via upload

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -84,15 +84,15 @@ class PlayState extends MusicBeatState
 
 	public static var ratingStuff:Array<Dynamic> = [
 		['You Suck!', 0.2], //From 0% to 19%
-		['Shit', 0.4], //From 20% to 39%
-		['Bad', 0.5], //From 40% to 49%
-		['Bruh', 0.6], //From 50% to 59%
+		['Damn It', 0.4], //From 20% to 39%
+		['Baddy', 0.5], //From 40% to 49%
+		['uh', 0.6], //From 50% to 59%
 		['Meh', 0.69], //From 60% to 68%
-		['Nice', 0.7], //69%
+		['Pretty Good', 0.7], //69%
 		['Good', 0.8], //From 70% to 79%
 		['Great', 0.9], //From 80% to 89%
-		['Sick!', 1], //From 90% to 99%
-		['Perfect!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
+		['Super!', 1], //From 90% to 99%
+		['Fanstatic!!!', 1] //The value on this one isn't used actually, since Perfect is always "1"
 	];
 
 	//event variables
@@ -264,15 +264,15 @@ class PlayState extends MusicBeatState
 	var tankmanRun:FlxTypedGroup<TankmenBG>;
 	var foregroundSprites:FlxTypedGroup<BGSprite>;
 
-	public var songScore:Int = 0;
+	public var songScores:Int = 0;
 	public var songHits:Int = 0;
-	public var songMisses:Int = 0;
+	public var songFails:Int = 0;
 	public var scoreTxt:FlxText;
 	var timeTxt:FlxText;
 	var scoreTxtTween:FlxTween;
 
-	public static var campaignScore:Int = 0;
-	public static var campaignMisses:Int = 0;
+	public static var campaignScores:Int = 0;
+	public static var campaignFails:Int = 0;
 	public static var seenCutscene:Bool = false;
 	public static var deathCounter:Int = 0;
 
